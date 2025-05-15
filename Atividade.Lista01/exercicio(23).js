3e3487792cc08da4b84f5e3dde84f77addbbdfdf
mariahelena1314 <mariahelenapereiradantas@gmail.com>
2025-05-15 18:34:29 +0000

    Atividade-Lista15.05

--- Atividade.Lista01/exercicio(23).js
+++ Atividade.Lista01/exercicio(23).js
@@ -20,3 +20,31 @@
 // : R$ 198,00
 // : R$ 902,00
 
+let bruto = 1000.00;
+let ir;
+let inss = bruto*0.10;
+let sindicato = bruto*0.03;
+let descontos;
+let liquido;
+
+if ( bruto <= 900){
+    ir = bruto;
+}
+else if ( bruto <= 1500){
+    ir = bruto *0.5
+}
+else if (bruto <= 2500){
+    ir = bruto*0.1
+}
+else {
+    ir = bruto * 0.2
+}
+
+descontos = ir + sindicato + inss;
+liquido = bruto - descontos;
+
+console.log(` Seu salario é: ${liquido} Reais`);
+
+
+
+
