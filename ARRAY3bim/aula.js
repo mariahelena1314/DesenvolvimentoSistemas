1128284bc13d0e5b1427b35171edffc633f6fa7f
mariahelena1314 <mariahelenapereiradantas@gmail.com>
2025-07-11 11:32:50 +0000

    array

---
+++ ARRAY3bim/aula.js
@@ -0,0 +1,30 @@
+        let frutas = [ "maçã", "kiwi", "morango", "abacaxi" ];
+        // console.log('tamanho do array:' + frutas.length);
+        
+        // console.log( 'Array original: ' + frutas);
+
+        // frutas.push("uva");
+        // console.log(frutas);
+
+        // frutas.push("banana");
+        // console.log(frutas);
+        // console.log(frutas.length);
+
+        frutas[2] = "caju";
+        frutas[4] = "pera";
+        frutas.push("seriguela");
+        frutas[5] = "romã";
+        console.log(frutas);
+        
+
+        frutas.pop(); 
+        frutas.pop(); 
+        frutas.pop(); 
+        
+        console.log(frutas);
+        
+    // console.log(frutas.sort());
+        
+        
+        
+        
---
+++ ARRAY3bim/carros.js
@@ -0,0 +1,35 @@
+let carros = [];
+
+// inserção elementos do array
+carros.push('corola');
+carros.push(' sw4 diamond');
+carros.push('onix');
+carros.push('fusca');
+
+// alterando elementos do array
+
+carros[2] = "amarok";
+
+// removendo o ultimo elemento
+carros.pop();
+
+
+// adicionando mais elementos no array
+
+carros.push('l200')
+carros.push('a3')
+carros.push('uno')
+
+// percorrendo o array
+
+for( i = 0; i < carros.length; i++){
+
+console.log("indice:" + i );
+console.log("valor:" + carros[i]);
+console.log();
+
+
+}
+
+
+
