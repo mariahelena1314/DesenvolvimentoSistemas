986ca3a701b0de7c40f7ab62f9d7e239114bc0a8
mariahelena1314 <mariahelenapereiradantas@gmail.com>
2025-08-15 11:56:07 +0000

    array15.08

--- ARRAY3bim/projetoUM/script.js
+++ ARRAY3bim/projetoUM/script.js
@@ -1,30 +1,49 @@
 let array = [];
 
+console.log("tipo do meu array: " + typeof(array));
 
-console.log("Tipo do meu array:" + typeof(array));
-
-function adicionarnumero() {
+function adiciornarNumero(){
     let input = document.querySelector('#numero');
-    let n = parseInt(input.Value);
-    let exibirArray = document.querySelector('exibir-array');
-
-    // se n for não numerico, mostrara um alert e encerrara a função
-
-    if(isNaN(n)){
-    alert('digite um numero válido!');
-    return;
+    let n = parseInt(input.value);
+    // alert(n)
+    let exibirArray = document.querySelector
+    ('#exibir-array');
+    // se N for não numerico.mostrara um alert e encerrara a funçao
+    if (isNaN(n)){
+        alert('digite um número valido!');
+        return;
     }
-    
     array.push(n);
 
-    // limpar o campo de entrada 
-
-    input.Value = '';
+    // limpar campo de entrada
+    input.value = '';
 
     console.log(array);
-
-    exibirArray.innerHTML = `Array: ${array} Tamanho do array ${array.length}`;
-    
+    exibirArray.innerHTML = `array: ${array} <br/>tamanho do array ${array.length}`;
 }
 
 
+function RemoverNumero() {
+    let input = document.querySelector('#numero1');
+    let n = parseInt(input.value);
+    let exibirArray = document.querySelector
+    ('#exibir-array');
+
+    if (isNaN(n)) {
+        alert('digite um número valido!');
+        return;
+    }
+
+    let indexN = array.indexOf(n);
+
+    if(indexN == -1){
+        alert ('esse numero nao esta no array ')
+    }
+    array.splice(indexN, 1);
+
+    // limpar campo de entrada
+    input.value = '';
+
+    console.log(array);
+    exibirArray.innerHTML = `array: ${array} <br/>tamanho do array ${array.length}`;
+}
